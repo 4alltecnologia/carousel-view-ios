8c005428cb05a9ce3e96b7953e27fc800a6f4e34
Matheus Alano <matheus.dias@4all.com>
2018-02-21 09:52:36 -0300

    Project renamed

---
+++ CarouselView/CarouselView.h
@@ -0,0 +1,19 @@
+//
+//  CarouselView.h
+//  CarouselView
+//
+//  Created by Matheus Alves Alano Dias on 21/02/2018.
+//  Copyright © 2018 4All. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarouselView.
+FOUNDATION_EXPORT double CarouselViewVersionNumber;
+
+//! Project version string for CarouselView.
+FOUNDATION_EXPORT const unsigned char CarouselViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarouselView/PublicHeader.h>
+
+
