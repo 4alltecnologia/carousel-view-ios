5764b18bf9630de3ee9e54cce4f462405043f16b
Matheus Alano <matheus.dias@4all.com>
2018-02-21 09:42:19 -0300

    Project created

---
+++ CarouselCollectionView/CarouselCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  CarouselCollectionView.h
+//  CarouselCollectionView
+//
+//  Created by Matheus Alves Alano Dias on 21/02/2018.
+//  Copyright © 2018 4All. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarouselCollectionView.
+FOUNDATION_EXPORT double CarouselCollectionViewVersionNumber;
+
+//! Project version string for CarouselCollectionView.
+FOUNDATION_EXPORT const unsigned char CarouselCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarouselCollectionView/PublicHeader.h>
+
+
